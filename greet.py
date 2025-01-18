4d460ad26742f6bbc1af6eecbbff828cdc618586
Balaji-Opula <balajiklnce@gmail.com>
2025-01-18 18:56:25 +0530

    feat : added list_prininting function

--- greet.py
+++ greet.py
@@ -1,5 +1,13 @@
 def main(user: str):
     print(f"Welcome Mr/Ms {user} to python learning scope...")
 
+def list_printing():
+    data = ["Python","Assembly","React","Database"]
+
+    for x in data:
+        print(x, end=' ') # to print details in same line
+
+
 if __name__ == "__main__":
     main(user="Balaji")
+    list_printing()
